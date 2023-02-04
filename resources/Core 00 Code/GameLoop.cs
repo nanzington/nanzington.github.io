0d5fa8d581ebb45470cc719b908f65d0ae24a721
jmsjtech <jmsjunior96@gmail.com>
2023-02-04 14:38:19 -0600

    Core 01 - First Draft
    Wrote up Core Chapter 1, and changed some of the formatting to (hopefully) look a bit better.

---
+++ resources/Core 00 Code/GameLoop.cs
@@ -0,0 +1,40 @@
+﻿using SadConsole; 
+using SadRogue.Primitives;
+using Console = SadConsole.Console;
+
+namespace SadTutorial {
+    class GameLoop { 
+        public const int GameWidth = 160;
+        public const int GameHeight = 50;
+        public const int MapWidth = 70;
+        public const int MapHeight = 43;
+
+        public static SadFont SquareFont;
+
+        public static Console startingConsole;
+
+        static void Main(string[] args) {
+            // Setup the engine and create the main window. 
+            Game.Create(GameWidth, GameHeight, "./fonts/ThinExtended.font");
+
+            // Hook the start event so we can add consoles to the system.
+            Game.Instance.OnStart = Init;
+
+            // Start the game.
+            Game.Instance.Run();
+            Game.Instance.Dispose();
+        }
+
+        private static void Init() {
+            // Load up our square font (CheepicusExtended) and save it for later - our game map will use it, while the rest defaults to our thin font.
+            SquareFont = (SadFont)GameHost.Instance.LoadFont("./fonts/CheepicusExtended.font");
+
+            // Any startup code for your game. We will use an example console for now
+            startingConsole = (Console) GameHost.Instance.Screen; 
+            startingConsole.Print(6, 5, "Hello World", Color.White);
+
+            // Set the window title to whatever you like, probably the name of your project.
+            Game.Instance.MonoGameInstance.Window.Title = "SadTutorial";
+        }
+    }
+}
---
+++ resources/Core 01 Code/UI/UIManager.cs
@@ -0,0 +1,47 @@
+﻿using SadConsole;
+using SadConsole.UI;
+using SadRogue.Primitives; 
+
+namespace SadTutorial.UI {
+    public class UIManager : ScreenObject {
+        public SadConsole.UI.Colors CustomColors;
+        public Dictionary<string, InstantUI> Interfaces = new();
+
+        public UIManager() {
+            IsVisible = true;
+            IsFocused = true;
+            UseMouse = true;
+            Parent = GameHost.Instance.Screen;
+        }
+
+        public override void Update(TimeSpan timeElapsed) {
+            foreach(KeyValuePair<string, InstantUI> kv in Interfaces) {
+                if (kv.Value.Win.IsVisible) {
+                    kv.Value.Update();
+                    kv.Value.Input();
+                }
+            }
+
+
+            Helper.ClearKeys();
+            base.Update(timeElapsed);
+        }
+
+        public void Init() {
+            SetupCustomColors();
+
+            UI_Sidebar Sidebar = new(40, 50);
+        }
+
+
+        private void SetupCustomColors() {
+            CustomColors = SadConsole.UI.Colors.CreateAnsi();
+            CustomColors.ControlHostBackground = new AdjustableColor(Color.Black, "Black");
+            CustomColors.Lines = new AdjustableColor(Color.White, "White");
+            CustomColors.Title = new AdjustableColor(Color.White, "White");
+
+            CustomColors.RebuildAppearances();
+            SadConsole.UI.Themes.Library.Default.Colors = CustomColors;
+        }
+    }
+}
